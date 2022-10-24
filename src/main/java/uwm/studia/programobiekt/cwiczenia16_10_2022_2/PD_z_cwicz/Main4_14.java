9bb75a8998eb4f3a0d10e133b391264f663662a5
mivhal07 <michaldob07@gmail.com>
2022-10-24 11:48:13 +0200

    PD z ćw 16.10.2022

---
+++ src/main/java/uwm/studia/programobiekt/cwiczenia16_10_2022_2/PD_z_cwicz/Main4_14.java
@@ -0,0 +1,28 @@
+package uwm.studia.programobiekt.cwiczenia16_10_2022_2.PD_z_cwicz;
+
+import java.time.LocalDate;
+
+public class Main4_14 {
+
+    public static void main(String[] args) {
+        String[] strings = {"KOMG-2002", "BH-2010", "DUSQ-2016", "LUAN-2013", "PANT-2000"};
+        String[] stringY = new String[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            stringY[i] = strings[i].split("-")[1];
+        }
+        int[] intY = new int[stringY.length];
+        for (int i = 0; i < intY.length; i++) {
+            intY[i] = Integer.parseInt(stringY[i]);
+        }
+        LocalDate localDate = LocalDate.now();
+        int year = localDate.getYear();
+
+        int[] time = new int[intY.length];
+        for (int i = 0; i < intY.length; i++) {
+            time[i] = year - intY[i];
+        }
+        for (int i = 0; i < strings.length; i++) {
+            System.out.println("Sprzęt: " + strings[i] + " ma aktualnie: " + time[i] + " lat");
+        }
+    }
+}
